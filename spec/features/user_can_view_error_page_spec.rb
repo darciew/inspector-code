a2fef01ff5ddfd36dafd0dd2a352c9d938e405c1
Darcie <darciewalsh@hotmail.com>
2018-12-09 14:26:30 +0000

    Errorpage - can see button to return to homepage

--- spec/features/user_can_view_error_page_spec.rb
+++ spec/features/user_can_view_error_page_spec.rb
@@ -8,4 +8,12 @@ feature 'Error Page' do
 
     expect(page).to have_css('.error-content')
   end
+
+  scenario 'Can see a button that would return them to the homepage' do
+    visit '/'
+    fill_in 'username', with: 'bad user name'
+    click_on 'Submit'
+
+    expect(page).to have_selector("input[class='return-home']")
+  end
 end
