08371a4fbb140d926b223b38796a3c23c93d5ef5
Darcie <darciewalsh@hotmail.com>
2018-12-09 12:21:28 +0000

    User is redirected to main page from results page

--- spec/features/user_can_return_to_homepage_spec.rb
+++ spec/features/user_can_return_to_homepage_spec.rb
@@ -6,4 +6,11 @@ feature 'Return to Homepage' do
 
     expect(page).to have_selector("input[class='return-home']")
   end
+
+  scenario 'Can click back to the homepage from the /results page' do
+    fill_in_form
+    click_on 'Try Another?'
+
+    expect(page).to have_current_path '/'
+  end
 end
